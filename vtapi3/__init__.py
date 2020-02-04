dcb2bb4432db9fa0ac8e001bfb22f3e1c2ba0b02
drobotun <drobotun@xakep.ru>
2020-02-05 00:28:37 +0300

    fixed __init__.py

--- vtapi3/__init__.py
+++ vtapi3/__init__.py
@@ -1,5 +1,12 @@
 __title__ = 'vtapi3'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __author__ = 'Evgeny Drobotun'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (C) 2020 Evgeny Drobotun'
+
+from .vtapi3 import (VirusTotalAPI, VirusTotalAPIFiles, VirusTotalAPIUrls, VirusTotalAPIDomains,
+                     VirusTotalAPIIPAddresses, VirusTotalAPIAnalyses, VirusTotalAPIError,
+                     get_environment_api_key, get_file_id_to_analyse, get_file_scan_report,
+                     get_file_analyse_report, get_hash_report, get_url_id_to_analyse,
+                     get_url_scan_report, get_url_analyse_report, get_ip_report,
+                     get_domain_report)
