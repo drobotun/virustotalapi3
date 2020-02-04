58d33a9b121c41406847b5626c504e4c798a03e8
drobotun <drobotun@xakep.ru>
2020-02-05 02:20:46 +0300

    add main()

--- vtapi3/vtapi3.py
+++ vtapi3/vtapi3.py
@@ -30,7 +30,6 @@
 import os
 import sys
 import json
-import argparse
 import base64
 import hashlib
 import errno
@@ -1470,59 +1469,3 @@ def get_domain_report(domain, api_key):
             return 'HTTP error ' + str(vt_domain.get_last_http_error())
     except VirusTotalAPIError as err:
         return err
-
-def main():
-    print('\nThe vtapi3 package. Implements the VirusTotal service API functions (3 versions).')
-    print('MIT Copyright (c) 2020, Evgeny Drobotun\n')
-    try:
-        api_key = get_environment_api_key()
-    except VirusTotalAPIError as err:
-        print(err)
-        sys.exit()
-
-    parser = argparse.ArgumentParser(description='vtapi3 package options')
-    parser.add_argument('resource',
-                        help='Object that you want to analyse in VirusTotal (file, URL, IP address or domain)')
-    parser.add_argument('-fid', '--file-id', action='store_true', dest='file_id',
-                        help='Getting the identifier of the file for further analysis')
-    parser.add_argument('-fsr', '--file-scan-report', action='store_true', dest='file_scan_report',
-                        help='Getting a report on the results of scanning a file')
-    parser.add_argument('-far', '--file-analyse-report', action='store_true', dest='file_analyse_report',
-                        help='Getting a report on the results of file analysis (enabled by default)')
-    parser.add_argument('-hr', '--hash-report', action='store_true', dest='hash_report',
-                        help='Getting a report on the results of analyzing a file by its hash (SHA256, SHA1 or MD5)')
-    parser.add_argument('-uid', '--url-id', action='store_true', dest='url_id',
-                        help = 'Getting the identifier of the URL for further analysis')
-    parser.add_argument('-usr', '--url-scan-report', action='store_true', dest='url_scan_report',
-                        help = 'Getting a report on the results of scanning a URL')
-    parser.add_argument('-uar', '--url-analyse-report', action='store_true', dest='url_analyse_report',
-                        help='Getting a report on the results of URL analysis')
-    parser.add_argument('-ipr', '--ip-report', action='store_true', dest='ip_report',
-                        help='Getting a report on the results of IP address analysis')
-    parser.add_argument('-dr', '--domain-report', action='store_true', dest='domain_report',
-                        help='Getting a report on the results of domain analysis')
-    options = parser.parse_args()
-    
-    if options.file_id:
-        print(get_file_id_to_analyse(options.resource, api_key))
-    elif options.file_scan_report:
-        print(get_file_scan_report(options.resource, api_key))
-    elif options.file_analyse_report:
-        print(get_file_analyse_report(options.resource, api_key))
-    elif options.hash_report:
-        print(get_hash_report(options.resource, api_key))
-    elif options.url_id:
-        print(get_url_id_to_analyse(options.resource, api_key))
-    elif options.url_scan_report:
-        print(get_url_scan_report(options.resource, api_key))
-    elif options.url_analyse_report:
-        print(get_url_analyse_report(options.resource, api_key))
-    elif options.ip_report:
-        print(get_ip_report(options.resource, api_key))
-    elif options.domain_report:
-        print(get_domain_report(options.resource, api_key))
-    else:
-        print(get_file_analyse_report(options.resource, api_key))
-
-if __name__ == '__main__':
-    main()
